46204328288817fea3302eae0788d397d62c4587
Ilie2 <147348536+Ilie2@users.noreply.github.com>
2024-08-10 21:11:13 +0300

    com

--- demo/src/main/java/com/example/demo/repository/ReviewRepos.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Review;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.List;
-
-public interface ReviewRepos extends MongoRepository<Review, Integer> {
-}
